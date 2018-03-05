4d991aa7cc4cc15c68bd6412b35f1fe37e3efacc
Nitesh Meshram <nitesh.meshram@v2solutions.com>
2018-03-05 17:45:08 +0530

    Added all project files

---
+++ DemoVisitorApp/HomeViewController.swift
@@ -0,0 +1,25 @@
+//
+//  HomeViewController.swift
+//  DemoVisitorApp
+//
+//  Created by Nitesh Meshram on 3/5/18.
+//  Copyright © 2018 V2Solutions. All rights reserved.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
