1f000545adb8e0c8400b057d4ff42cebb9394bb0
NiteshMeshram <meshram.nitesh@gmail.com>
2018-04-22 00:28:00 +0530

    Added assets

--- DemoVisitorApp/UserViewController.swift
+++ DemoVisitorApp/UserViewController.swift
@@ -13,9 +13,39 @@ import UIKit
 
 class UserViewController: UIViewController {
     
+    @IBOutlet weak var personToMeetTextField: UITextField!
+    @IBOutlet weak var companyTextField: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var lastNameTextField: UITextField!
+    @IBOutlet weak var firstNameTextField: UITextField!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let placeHolderColor = UIColor(red: 138.0/255.0, green: 138.0/255.0, blue: 138.0/255.0, alpha: 1.0)
+        
+        personToMeetTextField.setBottomBorder()
+        personToMeetTextField.attributedPlaceholder = NSAttributedString(string: "Person You Are Here To Meet",
+                                                               attributes: [NSAttributedStringKey.foregroundColor: placeHolderColor])
+        
+        
+        companyTextField.setBottomBorder()
+        companyTextField.attributedPlaceholder = NSAttributedString(string: "Your Company",
+                                                                         attributes: [NSAttributedStringKey.foregroundColor: placeHolderColor])
+        emailTextField.setBottomBorder()
+        emailTextField.attributedPlaceholder = NSAttributedString(string: "Your Email",
+                                                                    attributes: [NSAttributedStringKey.foregroundColor: placeHolderColor])
+        
+        lastNameTextField.setBottomBorder()
+        lastNameTextField.attributedPlaceholder = NSAttributedString(string: "Last Name",
+                                                                  attributes: [NSAttributedStringKey.foregroundColor: placeHolderColor])
+        firstNameTextField.setBottomBorder()
+        firstNameTextField.attributedPlaceholder = NSAttributedString(string: "First Name",
+                                                                     attributes: [NSAttributedStringKey.foregroundColor: placeHolderColor])
+        
+        
     }
     
     override func didReceiveMemoryWarning() {
@@ -33,4 +63,17 @@ class UserViewController: UIViewController {
         navigationController?.popViewController(animated: true)
     }
     
+}
+
+extension UITextField {
+    func setBottomBorder() {
+        self.borderStyle = .none
+        self.layer.backgroundColor = UIColor.white.cgColor
+        
+        self.layer.masksToBounds = false
+        self.layer.shadowColor = UIColor.gray.cgColor
+        self.layer.shadowOffset = CGSize(width: 0.0, height: 1.0)
+        self.layer.shadowOpacity = 1.0
+        self.layer.shadowRadius = 0.0
+    }
 }
